08ebe7f3d1998ed7c7e36820ba5e645f0e14e109
Marcelo W2N <marcelo@w2n.com.br>
2019-09-15 07:55:39 -0300

    Limpeza HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Campeonato.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Campeonato.Controllers
 {
