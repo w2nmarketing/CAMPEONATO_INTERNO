74aab18c9c03c910c50eb196e168e5a4a4c0daed
Marcelo W2N <marcelo@w2n.com.br>
2019-09-15 07:37:02 -0300

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Campeonato.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
