7a370eafc1bd23b8495c1c52c0d3de506d9345e5
Marcelo W2N <marcelo@w2n.com.br>
2019-09-15 07:46:26 -0300

    Criação da Class Enum Categoria

---
+++ Entidades/Enum/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Campeonato.Entidades.Enum
+{
+    enum Categoria : int
+    {
+
+        Sub7 = 1,
+        Sub9 = 2,
+        Sub12 = 3,
+        Sub15 = 4
+
+    }
+}
---
+++ Models/Times.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Campeonato.Models
+{
+    public class Times
+    {
+
+        public Enum Categoria { get; set; }
+        public string Nome { get; set; }
+        public string Escudo { get; set; }
+
+    }
+}
