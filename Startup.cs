0a4695f438b30278f2cad22821a311ae17ef6dbe
Marcelo W2N <marcelo@w2n.com.br>
2019-09-15 15:41:02 -0300

    Incluido a área admin

--- Startup.cs
+++ Startup.cs
@@ -58,7 +58,13 @@ namespace Campeonato
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
+
         }
     }
 }
